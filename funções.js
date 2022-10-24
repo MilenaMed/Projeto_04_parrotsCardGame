aa23198c03e36b9577849cda148ecc86e99db234
MilenaMed <milenamfm@gmail.com>
2022-10-24 00:39:43 -0300

    Virar cartas
    Virar cartas e distribuir imagens de forma aleatória
    Arrumei "else" que só repetia uma vez, agora vai até ficar válido.

--- funções.js
+++ funções.js
@@ -1,20 +1,78 @@
+// SELECIONAR A QUANTIDADE DE CARTAS
 let quantidade = prompt("Com quantas cartas que jogar?");
+
+const imgViradas = [
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    'metalparrot',
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"
+]
+imgViradas.sort(comparador);
+function comparador() { 
+	return Math.random() - 0.5;
+}
 
 function QuantidadeCartas() {
     if (quantidade % 2 == 0 && quantidade >= 4 && quantidade <= 14) {
-        for (i = 0; i < quantidade; i++) {
-            const lista = document.querySelector("ul")
-            console.log (lista)
+        const lista = document.querySelector("ul");
+        let elementosLista = [];
+
+        for (let i = 0; i < quantidade; i++) {
             const item = `
-                <li>
-                    <div onclick="virar(this)" class="card">
+                 <li>
+                 <div class="card" onclick="virarCarta(this)">
+                    <div class="front-face face">
                         <img src="imagens/back.png" class="imagem" />
                     </div>
-                </li>
-            `
-            lista.innerHTML += item
+                    <div class="back-face face">
+                    <img src="imagens/${imgViradas[Math.floor(i/2)]}.gif" class="imagem" />
+                    </div>
+                   </div>  
+                 </li>
+             `
+            elementosLista.push(item);
         }
+
+        elementosLista.sort(comparador);
+
+        for (let j = 0; j < elementosLista.length; j++) {
+            lista.innerHTML += elementosLista[j];
+        }
+
     } else {
-        return quantidade = prompt("Com quantas cartas que jogar?");
+        quantidade = prompt("Com quantas cartas que jogar?")
+        QuantidadeCartas()
     }
 }
+QuantidadeCartas();
+
+
+let contador = 0;
+
+function virarCarta(carta) {
+    carta.classList.toggle('virada');
+    contador++;
+
+    if (contador == 2) {
+        contador = 0;
+        // se as cartas forem diferentes
+        setTimeout(desvirar, 2000);
+
+    }
+}
+
+function desvirar() {
+    const cartas = document.querySelectorAll('.virada');
+    for (let i = 0; i < cartas.length; i++) {
+        cartas[i].classList.remove('virada');
+    }
+}
+
+
+
+// COLOCAR AS IMAGENS EMBARALHADAS
+
+   // return Math.random() - 0.5;
